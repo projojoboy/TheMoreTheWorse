8b00bb6fa607e57ac6bf0302c47e0f260ddee32e
Projojoboy <22048@ma-web.nl>
2018-01-16 22:57:01 +0100

    Added Basic Movement and Jumping

---
+++ TheMoreTheWorse/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+
+    public bool isGrounded;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+            isGrounded = false;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "GroundCheck")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * h, rb.velocity.y);
+    }
+}

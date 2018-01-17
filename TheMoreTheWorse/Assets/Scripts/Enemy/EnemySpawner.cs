50ef49158dfff61057b0a0d4fce6c30935b66baa
Projojoboy <22048@ma-web.nl>
2018-01-17 17:52:50 +0100

    Added Enemy Spawners

---
+++ TheMoreTheWorse/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] GameObject Enemy;
+
+    private float spawnDelay;
+    private bool spawn;
+
+	// Use this for initialization
+	void Start () {
+        spawn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (spawn)
+        {
+            spawnDelay = Random.Range(1f, 3f);
+            Instantiate(Enemy, transform.position, transform.rotation);
+            spawn = false;
+            StartCoroutine(SpawnEnemy());
+        }
+	}
+
+    IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        spawn = true;
+    }
+}

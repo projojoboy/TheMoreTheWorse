be157c9eb7dc288fb51503ad58b42ed25eb63511
Projojoboy <22048@ma-web.nl>
2018-01-19 23:34:51 +0100

    Fixed the wave spawning!

--- TheMoreTheWorse/Assets/Scripts/Enemy/EnemySpawner.cs
+++ TheMoreTheWorse/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,30 +4,114 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 
-    [SerializeField] GameObject Enemy;
+    public enum SpawnState { SPAWNING, WAITING, COUNTING};
 
-    private float spawnDelay;
-    private bool spawn;
-
-	// Use this for initialization
-	void Start () {
-        spawn = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (spawn)
-        {
-            spawnDelay = Random.Range(3f, 5f);
-            Instantiate(Enemy, transform.position, transform.rotation);
-            spawn = false;
-            StartCoroutine(SpawnEnemy());
-        }
-	}
-
-    IEnumerator SpawnEnemy()
+    [System.Serializable]
+    public class Wave
     {
-        yield return new WaitForSeconds(spawnDelay);
-        spawn = true;
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 5f;
+    private float waveCountDown;
+
+    private float searchCountDown = 1;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    private void Start()
+    {
+        waveCountDown = timeBetweenWaves;
+    }
+
+    private void Update()
+    {
+        if(state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive())
+            {
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if(waveCountDown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountDown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed!");
+
+        state = SpawnState.COUNTING;
+        waveCountDown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("Yo won tha game! restarting!");
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountDown -= Time.deltaTime;
+        if (searchCountDown <= 0f)
+        {
+            searchCountDown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning wave: " + _wave.name);
+        state = SpawnState.SPAWNING;
+
+        for(int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Debug.Log("Spawning enemy: " + _enemy.name);
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(_enemy, _sp.position, _sp.rotation);
     }
 }
---
+++ TheMoreTheWorse/Assets/Scripts/GameControllers/WaveController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+    public int wave;
+    public int amountMonSpawn;
+    public int amountMonsTillNextWave = 5;
+
+    private int bossWave;
+
+    public bool spawnBoss;
+    public bool spawnNextWave;
+
+    [SerializeField] Text waveText;
+
+	// Use this for initialization
+	void Start () {
+        wave = 1;
+        amountMonSpawn = 5;
+        bossWave = 5;
+        spawnBoss = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AmountSpawnWave();
+        waveText.text = "Wave: " + wave;
+	}
+
+    void AmountSpawnWave()
+    {
+        if(spawnNextWave == true)
+        {
+            amountMonsTillNextWave = amountMonSpawn;
+        }
+
+        if(wave == bossWave)
+        {
+            amountMonSpawn += 5;
+            bossWave += 5;
+        }
+
+        if(amountMonsTillNextWave == 0)
+        {
+            StartCoroutine(WaitTillNextWave());
+        }
+    }
+
+    IEnumerator WaitTillNextWave()
+    {
+        yield return new WaitForSeconds(3);
+        spawnNextWave = true;
+        wave++;
+    }
+}
